db07bbc64d4a9a8b17c17c9e80607aaf7e8774e0
vrrgithub1 <106780561+vrrgithub1@users.noreply.github.com>
2026-02-05 16:39:50 -0500

    update in config file

--- src/config.py
+++ src/config.py
@@ -8,13 +8,14 @@ from pathlib import Path
 # Define the base directory of the application
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 # Data Paths
-DATA_DIR = BASE_DIR / "data"
+DATA_DIR = PROJECT_ROOT / "data"
 DATABASE_PATH = DATA_DIR / "stock_risk_vault.db"
 
 # Report Paths (Used by your Plotly logic)
-REPORT_DIR = BASE_DIR / "reports"
+REPORT_DIR = PROJECT_ROOT / "reports"
 
 # Ensure directories exist
 os.makedirs(DATA_DIR, exist_ok=True)
